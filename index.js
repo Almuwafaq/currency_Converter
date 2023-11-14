574daa07bec73159bcdd9b73f1f2faeb08fa14f8
Almuwafaq <toriolataofeekolamilekan@gmail.com>
2023-11-14 22:50:17 +0100

    added the Api to it

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 const input = document.querySelector("#text-Input")
+const NairaInput = document.querySelector("#NN")
+const DollarInput = document.querySelector("#DD")
+const btn = document.querySelector("#btn")
+const h3 = document.createElement("h3") 
+
+const Exchange = async ()=> {
+    const response = await fetch("https://api.coindesk.com/v1/bpi/currentprice.json")
+    const ExtoJAva = await response.json()
+}

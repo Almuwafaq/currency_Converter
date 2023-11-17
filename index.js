b506be7a2436ec003cc3831db13cbfdd433b030e
Almuwafaq <toriolataofeekolamilekan@gmail.com>
2023-11-17 10:05:24 +0100

    added the dollar and zar rand to it

--- index.js
+++ index.js
@@ -16,27 +16,33 @@ const DOllARexchange = async () => {
   const dollValue = toJAva;
   return dollValue.rates.ZAR;
 };
+const BaseExchange = async () => {
+    const response = await fetch("https://api.frankfurter.app/latest?from=ZAR");
+    const ExtoJAva = await response.json();
+    const zarValue = ExtoJAva;
+    return zarValue.base;
+  };
+  const BaseDOllARexchange = async () => {
+    const response = await fetch("https://api.frankfurter.app/latest?from=USD");
+    const toJAva = await response.json();
+    const dollValue = toJAva;
+    return dollValue.base;
+  };
 ZARAN.addEventListener("click", async () => {
   let ranValue = await Exchange();
+  let ZArbased = await BaseExchange()
   btn.addEventListener("click", async () => {
-    h3.textContent = input.value * ranValue;
+    h3.textContent = input.value * ranValue + ZArbased;
+    input.value = ""
   });
 });
 
 Dollar.addEventListener("click", async () => {
+    let Dollbased = await BaseDOllARexchange() 
   let dollarValue = await DOllARexchange();
   btn.addEventListener("click", async () => {
-    h3.textContent = input.value * dollarValue;
+    h3.textContent = input.value * dollarValue +  Dollbased;
+    input.value = ""
   });
 });
-// btn.addEventListener('click', async () =>{
-//     let value =
-// })
 
-// const api = fetch("https://api.frankfurter.app/latest?from=USD")
-// const apis = ()=> {
-//     api
-//    .then((res)=> res.json() )
-//    .then((result)=> console.log(result))
-// }
-// apis()
